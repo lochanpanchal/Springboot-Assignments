dbca08407b9209aa2fef9e3dbce57f6f9bba236f
LOCHAN PANCHAL <75006690+lochanpanchal@users.noreply.github.com>
2023-06-22 18:30:32 +0530

    learn method 2 to fetch data
    using java

---
+++ fetch data using java 2/src/Main.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        HttpClient client=HttpClient.newBuilder().build();
+        HttpRequest request=HttpRequest.newBuilder().GET().uri(URI.create("https://api.zippopotam.us/us/33162")).build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.statusCode());
+        System.out.println(response.body());
+
+    }
+}
