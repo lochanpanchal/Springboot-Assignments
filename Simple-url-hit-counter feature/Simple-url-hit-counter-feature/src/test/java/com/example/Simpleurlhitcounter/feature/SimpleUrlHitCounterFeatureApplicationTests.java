d25db0384049198c5968c0351d033a2f84560487
LOCHAN PANCHAL <75006690+lochanpanchal@users.noreply.github.com>
2023-06-24 17:00:12 +0530

    added url-hit-counter
    learn how to update hit button on backend

---
+++ Simple-url-hit-counter feature/Simple-url-hit-counter-feature/src/test/java/com/example/Simpleurlhitcounter/feature/SimpleUrlHitCounterFeatureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Simpleurlhitcounter.feature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleUrlHitCounterFeatureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
